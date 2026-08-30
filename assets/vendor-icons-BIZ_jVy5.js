4800d1ca18a354e8c92309f73bce9ae5c6c788b5
Nazar <nazar.honcharuk1@gmail.com>
2026-08-30 22:12:29 +0200

    Updates

--- assets/vendor-icons-BIZ_jVy5.js
+++ assets/vendor-icons-BIZ_jVy5.js
@@ -1,4 +1,4 @@
-import{r as s}from"./vendor-CjG7vb3n.js";/**
+import{r as s}from"./vendor-BakI2RRH.js";/**
  * @license lucide-react v1.23.0 - ISC
  *
  * This source code is licensed under the ISC license.
